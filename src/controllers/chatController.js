6dcbdfd4819df1572bd21d40623d9e7f0132ba5c
Toushif Khan <toushifjh0756@gmail.com>
2026-07-26 16:08:01 +0530

    Complete CRUD on chat

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,75 +1,242 @@
-const Chat = require ("../models/Chat");
-const Workspace = require("../models/Workspace")
+const Chat = require("../models/Chat");
+const Workspace = require("../models/Workspace");
 
-const createChat = async(req,res)=>{
-
-  try { 
+const createChat = async (req, res) => {
+  try {
     const { title } = req.body;
-    const {workspaceId} = req.params;  // on clicking sends which workspace id acc. to the workspace clicked
+    const { workspaceId } = req.params; // on clicking sends which workspace id acc. to the workspace clicked
 
-        if(!title){
-            return res.status(400).json({
-                message: "title required"
-            })
-        };
-
-        const newChat = await Chat.create({
-           workspace : workspaceId,
-            title,
-        });
-
-        const createdChat ={
-            message: "Chat created",
-            chat : {
-               title: newChat.title
-                }
-        }
-
-        return res.status(201).json({
-            message:"Created Successfully",
-           createdChat
-        })
-            }catch(error){
-                console.log(error);
-                return res.status(500).json({
-                    message: "Internal error"
-                })
-            }
-}
-
-
-    const getChat = async(req,res)=>{
-        
-      try { 
-        const {workspaceId} = req.params;
-
-        const matchworkspace = await Workspace.findById( workspaceId)
-
-        if(!workspace){
-            return res.status(404).json({
-                message : "not found"
-            })
-        }
-
-       if(matchworkspace.owner.toString() !== req.currentUser.id){
-                return  res.status(403).json({
-                    message : "Forbidden"
-                })
-       }
-
-        const chats = await Chat.find({workspace : workspaceId})
-            return res.status(200).json({
-                message : "Fetched Successfully",
-                chats
-            })
-
-        } catch(error){
-            console.log(error);
-            return res.status(500).json({
-                message : "Internal error"
-            })
-        }
+    if (!title) {
+      return res.status(400).json({
+        message: "title required",
+      });
     }
 
+    const matchWorkspace = await Workspace.findById(workspaceId); // workspace id is unique inside it chats live W1-c1,c2,c3  W2- c1,c2,c3
 
-    module.exports = {createChat , getChat}
+    if (!matchWorkspace) {
+      //we verify cause our route is /api/workspaces:workspaceID/chats/:id
+      return res.status(404).json({
+        //i want to update chat :id inside workspaec :workspaceId  if someone sends put /w1/c2 request all c2 will be changed that doest belong to w1
+        message: "not found",
+      });
+    }
+
+    if (matchWorkspace.owner.toString() !== req.currentUser.id) {
+      // to check the workspcae belongs to current user or not  workspace.owner stores the  user id and req.currentuser.id is provided by authMiddleware
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+
+    const newChat = await Chat.create({
+      workspace: workspaceId,
+      title,
+    });
+
+    const createdChat = {
+      message: "Chat created",
+      chat: {
+        _id: newChat._id,
+        title: newChat.title,
+      },
+    };
+
+    return res.status(201).json({
+      message: "Created Successfully",
+      createdChat,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal error",
+    });
+  }
+};
+
+const getChat = async (req, res) => {
+  try {
+    const { workspaceId } = req.params;
+
+    const matchWorkspace = await Workspace.findById(workspaceId);
+
+    if (!matchWorkspace) {
+      return res.status(404).json({
+        message: "not found",
+      });
+    }
+
+    if (matchWorkspace.owner.toString() !== req.currentUser.id) {
+      // to check the workspace belongs to current user or nnt
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+
+    const chats = await Chat.find({ workspace: workspaceId }); // we didnt use chhat to workspace security check because
+    //query already gurantee every chat belong to requested woorkspace {worspace : workspaceId}
+
+    return res.status(200).json({
+      message: "Fetched Successfully",
+      chats,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal error",
+    });
+  }
+};
+
+const getOneChat = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { workspaceId } = req.params;
+
+    const matchWorkspace = await Workspace.findbById(workspaceId);
+
+    if (!matchWorkspace) {
+      return res.status(404).json({
+        message: "Not found",
+      });
+    }
+
+    if (matchWorkspace.owner.toString() !== req.currentUser.id) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+
+    const chat = await Chat.findById(id);
+
+    if (!chat) {
+      return res.status(404).json({
+        message: "Not found",
+      });
+    }
+
+    if (chat.workspace.toString() !== workspaceId) {
+      //to verify that if the chat is present, also then the chat belongs to same workspace or someother workspace ,else if someone does /workspaces/W1/chats/C5
+      //where C5 actually belongs to W2,  API would return it.
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+
+    return res.status(200).json({
+      message: "Fetched Successfully",
+      chat,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal error",
+    });
+  }
+};
+
+const updateChat = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { workspaceId } = req.params;
+    const { title } = req.body;
+
+    if (!title) {
+      return res.status(400).json({
+        message: "Title required",
+      });
+    }
+
+    const matchWorkspace = await Workspace.findById(workspaceId); //the request will be put/workspaces/workspaceId/:id
+    //this is to verfiy that the worspaceId that came, there is a workspace with that Id
+    if (!matchWorkspace) {
+      return res.status(404).json({
+        message: "Not Found",
+      });
+    }
+
+    if (matchWorkspace.owner.toString() !== req.currentUser.id) {
+      // this is to verify the workspace requested belongs to current loggedin user
+      return res.status(403).json({
+        // owner has UserId , authmiddleware gives the currentUser Id
+        message: "forbidden",
+      });
+    }
+
+    const chat = await Chat.findOne({
+      // this is short version before this we did something chat.Workspaace != workspace
+      _id: id, //to check if chat belong to the workspace or not
+      workspace: workspaceId, // now findone will look for chat with the requested chat id and workspace id
+    });
+
+    if (!chat) {
+      return res.status(404).json({
+        message: "Not found",
+      });
+    }
+
+    chat.title = title;
+
+    await chat.save();
+
+    const updatedChat = {
+      _id: chat._id,
+      title: chat.title,
+    };
+
+    return res.status(200).json({
+      message: "updated sucessfully",
+      updatedChat,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal error",
+    });
+  }
+};
+
+const deleteChat = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { workspaceId } = req.params;
+
+    const matchWorkspace = await Workspace.findById(workspaceId);
+
+    if (!matchWorkspace) {
+      return res.status(404).json({
+        message: "not found",
+      });
+    }
+
+    if (matchWorkspace.owner.toString() !== req.currentUser.id) {
+      return res.status(403).json({
+        message: "forbidden",
+      });
+    }
+
+    const chat = await Chat.findOne({
+      _id: id,
+      workspace: workspaceId,
+    });
+
+    if (!chat) {
+      return res.status(404).json({
+        message: "not found",
+      });
+    }
+
+    await chat.deleteOne();
+
+    return res.status(200).json({
+      message: "deleted sucessfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal error",
+    });
+  }
+};
+
+module.exports = { createChat, getChat, getOneChat, updateChat, deleteChat };
--- src/controllers/workspaceController.js
+++ src/controllers/workspaceController.js
@@ -1,174 +1,166 @@
- const Workspace = require("../models/Workspace")
+const Workspace = require("../models/Workspace");
 
+const createWorkspace = async (req, res) => {
+  try {
+    const { name, icon } = req.body;
 
-const createWorkspace = async(req,res)=>{
-    try{
-        const {name , icon} = req.body;
-
-        if(!name){
-            return res.status(400).json({
-                message : "name required"
-            })
-        }
-
-      const newWorkspace = await Workspace.create({
-                
-                owner : req.currentUser.id,  // user id sent by middleware attached to req object              
-                name: name,
-                icon: icon
-       });
-
-       const createdWorkspace = {
-            message : "Workspace Created Successfully",
-
-            workspace:{
-              _id : newWorkspace._id,  //workspace id created by mongodb
-             name : newWorkspace.name,
-             icon : newWorkspace.icon
-            }
-       };
-
-       return res.status(201).json(createdWorkspace);
-
-        }catch(error){
-            console.log(error);
-            return res.status(500).json({
-                message : "Internal error"
-            })
-        }
-
-}
-
-
-
-    const getWorkspace = async(req,res)=>{
-
-   try {
-        const owner = req.currentUser.id;
-
-    const workspaces = await Workspace.find({owner})
-
-        return res.status(200).json({
-            message : "workspaces fetched successfully",
-            workspaces
-        });
-    }catch(error){
-            console.log(error);
-            return res.status(500).json({
-                message : "Internal error"
-            })
+    if (!name) {
+      return res.status(400).json({
+        message: "name required",
+      });
     }
 
+    const newWorkspace = await Workspace.create({
+      owner: req.currentUser.id, // user id sent by middleware attached to req object
+      name: name,
+      icon: icon,
+    });
+
+    const createdWorkspace = {
+      message: "Workspace Created Successfully",
+
+      workspace: {
+        _id: newWorkspace._id, //workspace id created by mongodb
+        name: newWorkspace.name,
+        icon: newWorkspace.icon,
+      },
+    };
+
+    return res.status(201).json(createdWorkspace);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal error",
+    });
+  }
+};
+
+const getWorkspace = async (req, res) => {
+  try {
+    const owner = req.currentUser.id;
+
+    const workspaces = await Workspace.find({ owner }); //Rule of thumb
+    // Listing resources (find): If your query already filters by the logged-in user (or another ownership field), an additional ownership check is usually unnecessary.
+    // Workspace.find({ owner: req.currentUser.id })
+    // Fetching one resource by ID (findById): You must verify ownership after fetching it, because findById only checks _id, not who owns it.
+    return res.status(200).json({
+      message: "workspaces fetched successfully",
+      workspaces,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal error",
+    });
+  }
+};
+
+const getOneWorkspace = async (req, res) => {
+  try {
+    const { id } = req.params; // not req.params.id as we are destructuring not assigning if assigining then const id = req.params.id
+
+    const oneWorkspace = await Workspace.findById(id); // searches by Id assigned by mongodb to the workspace , on click frontend sends the _id assigned by mongodb and backend search with the same id
+    // why (id) & not ({id}) because findbyId looks for _id itself if we were doing  findone then findOne({_id : id}) means where _id = id
+    if (!oneWorkspace) {
+      return res.status(404).json({
+        message: "Not found",
+      });
     }
 
-    const getOneWorkspace  = async(req, res)=>{
-          try { 
-             const {id} = req.params; // not req.params.id as we are destructuring not assigning if assigining then const id = req.params.id
-
-             const oneWorkspace = await Workspace.findById(id);  // searches by Id assigned by mongodb to the workspace , on click frontend sends the _id assigned by mongodb and backend search with the same id
-                                                        // why (id) & not ({id}) because findbyId looks for _id itself if we were doing  findone then findOne({_id : id}) means where _id = id
-                if(!oneWorkspace){
-                    return res.status(404).json({
-                        message : "Not found"
-                })
-                }
-
-                if(oneWorkspace.owner.toString() !== req.currentUser.id){  // cause owner is a objectId(check in workspace Schema that'll be objectId("68..."), so, .tostring()
-                    return res.status(403).json({
-                        message : "Forbidden"   // object workspace  {_id:w1, owner: u1 , name : personal} this is to verify current logged in user and the requested workspace belong to same person 
-                    })
-                }
-
-                return res.status(200).json(oneWorkspace);
-
-            }catch(error){
-                    console.log(error);
-                    return res.status(500).json({
-                        message: "Internal Error"
-                    })
-            }
-
+    if (oneWorkspace.owner.toString() !== req.currentUser.id) {
+      // cause owner is a objectId(check in workspace Schema that'll be objectId("68..."), so, .tostring()
+      return res.status(403).json({
+        message: "Forbidden", // object workspace  {_id:w1, owner: u1 , name : personal} this is to verify current logged in user and the requested workspace belong to same person
+      });
     }
 
+    return res.status(200).json(oneWorkspace);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Error",
+    });
+  }
+};
 
+const updateWorspace = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-    const updateWorspace = async(req,res)=>{
-            try {
-                    const {id} = req.params;
+    const { name, icon } = req.body;
 
-                    const {name , icon} = req. body;
+    const matchWorkspace = await Workspace.findById(id);
 
-                const matchWorkspace = await Workspace.findById(id);
+    if (!matchWorkspace) {
+      return res.status(404).json({
+        message: "Not found",
+      });
+    }
 
-                if(!matchWorkspace){
-                     return res.status(404).json({
-                        message : "Not found"
-                })
-                }
+    if (matchWorkspace.owner.toString() !== req.currentUser.id) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
 
-                if(matchWorkspace.owner.toString() !== req.currentUser.id){
-                    return res.status(403).json({
-                        message : "Forbidden"
-                    })
-                }
+    matchWorkspace.name = name;
+    matchWorkspace.icon = icon;
 
-                matchWorkspace.name = name;
-                matchWorkspace.icon = icon;
+    await matchWorkspace.save(); // after changes .save saves document
 
-                await matchWorkspace.save(); // after changes .save saves document
+    const updatedWorkspace = {
+      _id: matchWorkspace._id,
+      name: matchWorkspace.name,
+      icon: matchWorkspace.icon,
+    };
 
-                const updatedWorkspace = {
-                    _id : matchWorkspace._id,
-                    name : matchWorkspace.name,
-                    icon : matchWorkspace.icon
-                }
+    return res.status(200).json({
+      message: "updated",
+      updatedWorkspace,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Error",
+    });
+  }
+};
 
-            return res.status(200).json({
-                message : "updated",
-                updatedWorkspace    
-            })
+const deleteWorkspace = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-                
-            } catch (error) {
-                console.log(error);
-                return res.status(500).json({
-                        message: "Internal Error",
-                    })
-            }
-        }
+    const matchWorkspace = await Workspace.findById(id);
 
-     const deleteWorkspace = async(req, res)=>{
-               try {
-                const { id } = req.params;
-                
-                const matchWorkspace = await Workspace.findById(id);
+    if (!matchWorkspace) {
+      return res.status(404).json({
+        message: "not found",
+      });
+    }
 
-                if(!matchWorkspace){
-                    return res.status(404).json({
-                        message:"not found"
-                    })
-                };
+    if (matchWorkspace.owner.toString() !== req.currentUser.id) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
 
-                    if(matchWorkspace.owner.toString() !== req.currentUser.id){
-                        return res.status(403).json({
-                            message:"Forbidden"
-                        })
-                    }
+    await matchWorkspace.deleteOne();
 
-                    await matchWorkspace.deleteOne();
+    return res.status(200).json({
+      message: "deleted succesfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal error",
+    });
+  }
+};
 
-                    return res.status(200).json({
-                        message:"deleted succesfully"
-                    })
-
-                }catch(error){
-                    console.log(error);
-                    return res.status(500).json({
-                            message: "Internal error"
-                    })
-                }
-
-                }
-    
-
-module.exports = {createWorkspace , getWorkspace , getOneWorkspace , updateWorspace , deleteWorkspace };
+module.exports = {
+  createWorkspace,
+  getWorkspace,
+  getOneWorkspace,
+  updateWorspace,
+  deleteWorkspace,
+};
